a2a4c1f68afd87d8fb071bc319e382301023ce09
juan9900 <juanluislauretta@gmail.com>
2022-12-29 09:43:18 -0400

    removed loading screen

--- js/loading.js
+++ js/loading.js
@@ -1,8 +1,8 @@
-window.onload = function () {
-	let contenedor = document.getElementById('contenedor_loader');
-	// setTimeout(() =>{
-		contenedor.style.visibility = 'hidden';
-		contenedor.style.opacity = '0';
-	// },3000)
-	//Removed setTimeout to reduce load time 
-}
+// window.onload = function () {
+// 	let contenedor = document.getElementById('contenedor_loader');
+// 	// setTimeout(() =>{
+// 		contenedor.style.visibility = 'hidden';
+// 		contenedor.style.opacity = '0';
+// 	// },3000)
+// 	//Removed setTimeout to reduce load time 
+// }

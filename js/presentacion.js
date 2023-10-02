3bfe71ba844d4d85dfbb0de0650da94a2da5b46f
Juan <macbook@Macbook-Pro-de-Juan.local>
2023-10-02 09:54:00 -0400

    Se actualizo la lista de clientes y de presentacion

--- js/presentacion.js
+++ js/presentacion.js
@@ -1,86 +1,89 @@
-$(document).ready(function(){
-    $('.miniature-img-container').each(function(index, obj){
-        $(this).on('click',function(){
-            // Make all video modals dissapear
-            $('.modal-video').removeClass('modal-video-visible');
+$(document).ready(function () {
+  $(".miniature-img-container").each(function (index, obj) {
+    $(this).on("click", function () {
+      // Make all video modals dissapear
+      $(".modal-video").removeClass("modal-video-visible");
 
-        const videoName = $(this).parent().attr('id');
-        const video = $(`#presentation-video-${index+1}`);
-        // add display block to the video modal and then 1ms later, add opacity 1
-        $(`.${videoName}`).show();
-        setTimeout(()=>{
-            $(`.${videoName}`).addClass('modal-video-visible');
-        },1)
+      const videoName = $(this).parent().attr("id");
+      console.log("🚀 ~ file: presentacion.js:8 ~ videoName:", videoName);
+      const video = $(`#presentation-${videoName}`);
 
-        //Start video
-        $(video).get(0).play();
+      console.log("🚀 ~ file: presentacion.js:11 ~ video:", video);
+      // add display block to the video modal and then 1ms later, add opacity 1
 
-        // When click outside the video but in the modal, set opacity to 0 and display none
-        $(`.${videoName}`).on('click',function(){
-            $('.modal-video').removeClass('modal-video-visible');
-                setTimeout(()=>{
-                    $(`.${videoName}`).hide();
+      console.log(`.modal-${videoName}`);
+      $(`.modal-${videoName}`).show();
+      setTimeout(() => {
+        $(`.modal-${videoName}`).addClass("modal-video-visible");
+      }, 1);
 
-                },500);
-            $(video).get(0).pause();
-            $(video).get(0).currentTime = 0;
-        })
+      //Start video
+      $(video).get(0).play();
 
-        // When video ended, close modal 
-        $(video).on('ended',function(){
-            setTimeout(() => {
-                $('.modal-video').removeClass('modal-video-visible');
-                setTimeout(()=>{
-                    $(`.${videoName}`).hide();
+      // When click outside the video but in the modal, set opacity to 0 and display none
+      $(`.modal-${videoName}`).on("click", function () {
+        $(".modal-video").removeClass("modal-video-visible");
+        setTimeout(() => {
+          $(`.modal-${videoName}`).hide();
+        }, 500);
+        $(video).get(0).pause();
+        $(video).get(0).currentTime = 0;
+      });
 
-                },500);
+      // When video ended, close modal
+      $(video).on("ended", function () {
+        setTimeout(() => {
+          $(".modal-video").removeClass("modal-video-visible");
+          setTimeout(() => {
+            $(`.${videoName}`).hide();
+          }, 500);
+        }, 1000);
+      });
+    });
+  });
 
-            },1000);
-        })
+  // Handle brand click
+  $(".brand-container").each(function (index, obj) {
+    $(this).on("click", function () {
+      const brandName = $(this).find("p").text();
+      // Separate the product name words by - to use it in images src
+      const brandFileName = brandName
+        .replace(new RegExp(" ", "g"), "-")
+        .toLowerCase();
+      console.log(brandFileName);
 
-        })
-    })
+      const carouselIndicators = $(".carousel-indicators");
+      const myCarouselElement = $("#carousel-more-brand");
+      const carouselBrandImages = $(".more-brand-inner");
+      //Set the carousel back to position 0 when reopening the modal
+      const carousel = new bootstrap.Carousel(myCarouselElement).to(0);
+      $(carouselIndicators).empty();
+      const imagesQty = parseInt($(this).attr("data-bs-images"));
 
+      // Take the value of data-bs-images in each product, the value must be an integer
+      // this will create X number of buttons for each image of the product (data-bs-images)
+      for (let i = 0; i < imagesQty; i++) {
+        const newButton = `<button class="${
+          i === 0 ? "active" : ""
+        } indicator" aria-current="${
+          i === 0 ? true : false
+        }" type="button" data-bs-target="#carousel-more-brand" data-bs-slide-to="${i}" aria-label="Slide ${
+          i + 1
+        }"></button>`;
 
+        $(carouselIndicators).append(newButton);
+      }
 
-    // Handle brand click
-    $('.brand-container').each(function(index, obj){
-        $(this).on('click',function(){
-            const brandName = $(this).find('p').text();
-            // Separate the product name words by - to use it in images src
-            const brandFileName = brandName.replace(new RegExp(' ','g'),'-');
+      //Generate images
+      $(carouselBrandImages).empty();
+      for (let i = 1; i <= imagesQty; i++) {
+        $(carouselBrandImages).append(`
+                <div class="carousel-item ${i === 1 ? "active" : ""}">
+                    <img src="/img/presentacion/${brandFileName}-${i}.webp" loading="lazy" class="d-block more-product-img" alt="...">
+                </div>`);
+      }
 
-            const carouselIndicators = $('.carousel-indicators');
-            const myCarouselElement = $('#carousel-more-brand');
-            const carouselBrandImages = $('.more-brand-inner');
-            //Set the carousel back to position 0 when reopening the modal
-            const carousel = new bootstrap.Carousel(myCarouselElement).to(0);
-            $(carouselIndicators).empty();
-            const imagesQty = parseInt($(this).attr('data-bs-images'));
-
-            // Take the value of data-bs-images in each product, the value must be an integer
-            // this will create X number of buttons for each image of the product (data-bs-images)
-            for(let i = 0; i< imagesQty; i++){
-                const newButton = `<button class="${i===0 ? 'active' : '' } indicator" aria-current="${i===0?true:false}" type="button" data-bs-target="#carousel-more-brand" data-bs-slide-to="${i}" aria-label="Slide ${i+1}"></button>`;
-                
-                $(carouselIndicators).append(newButton);
-
-            }
-
-            //Generate images
-            console.log('generating image');
-            console.log(brandFileName);
-            $(carouselBrandImages).empty();
-            for(let i = 1; i<=imagesQty; i++){
-                console.log(`brandFileName-${i}`)
-                console.log(`generated: ${i}`);
-                $(carouselBrandImages).append(`
-                <div class="carousel-item ${i === 1 ? 'active' : ''}">
-                    <img src="/img/presentacion/${brandFileName}-${i}.jpg" class="d-block more-product-img" alt="...">
-                </div>`)
-            }
-
-            $('#brand-title').text(brandName);
-        })
-    })
-})
+      $("#brand-title").text(brandName);
+    });
+  });
+});
